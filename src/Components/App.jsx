8df8c4490c974fe54fd405481d532aeab459d5a0
mustafcode10 <mustafdiiriye@hotmail.com>
2024-08-24 15:25:52 -0500

    initial

--- src/Components/App.jsx
+++ src/Components/App.jsx
@@ -1,5 +1,5 @@
-import React, { useState, useEffect } from 'react';
-import Recipe from './Recipe';
+import React, { useState, useEffect } from "react";
+import Recipe from "./Recipe";
 
 export default function App() {
   const [recipes, setRecipes] = useState([]);
@@ -7,30 +7,36 @@ export default function App() {
   const [selectedCategory, setSelectedCategory] = useState("Seafood");
 
   useEffect(() => {
-    fetch("https://www.themealdb.com/api/json/v1/1/filter.php?c=" + selectedCategory)
-      .then((response) => response.json())
-      .then((data) => setRecipes(data.meals))
-      .catch((error) => console.error('Error:', error));
+    fetch(
+      "https://www.themealdb.com/api/json/v1/1/filter.php?c=" + selectedCategory
+    )
+      .then(response => response.json())
+      .then(data => setRecipes(data.meals))
+      .catch(error => console.error("Error:", error));
   }, [selectedCategory]);
 
   useEffect(() => {
     fetch("https://www.themealdb.com/api/json/v1/1/categories.php")
-      .then((response) => response.json())
-      .then((data) => setCategories(data.categories))
-      .catch((error) => console.error('Error:', error));
+      .then(response => response.json())
+      .then(data => setCategories(data.categories))
+      .catch(error => console.error("Error:", error));
   }, []);
 
-  const handleSelectChange = (event) => {
+  const handleSelectChange = event => {
     setSelectedCategory(event.target.value);
-  }
+  };
 
   return (
     <>
       <h1>My Favorite Recipes</h1>
-      <div className='categories'>
+      <div className="categories">
         <label htmlFor="categorySelect">Select Category: </label>
-        <select id="categorySelect" value={selectedCategory} onChange={handleSelectChange}>
-          {categories.map((category) => (
+        <select
+          id="categorySelect"
+          value={selectedCategory}
+          onChange={handleSelectChange}
+        >
+          {categories.map(category => (
             <option value={category.strCategory} key={category.idCategory}>
               {category.strCategory}
             </option>
@@ -38,7 +44,7 @@ export default function App() {
         </select>
       </div>
       <div className="container">
-        {recipes.map((recipe) => (
+        {recipes.map(recipe => (
           <Recipe data={recipe} key={recipe.idMeal} />
         ))}
       </div>
